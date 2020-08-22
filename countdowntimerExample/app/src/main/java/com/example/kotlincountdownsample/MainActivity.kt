f420c6ad7cee7ec2dbb28022179aacacd03f9598
htk007 <hasankaya921@hotmail.com>
2020-08-22 23:22:49 +0300

    kotlin examples push2

--- countdowntimerExample/app/src/main/java/com/example/kotlincountdownsample/MainActivity.kt
+++ countdowntimerExample/app/src/main/java/com/example/kotlincountdownsample/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.kotlincountdownsample
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var eetStartTime: EditText?=null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       eetStartTime = findViewById(R.id.etStartTime) as EditText
+    }
+
+    fun startTimer(view: View){
+        //geriye sayan sayaç.
+        //içerisinde bizim belirleyeceğimiz bir periyoyotta, mesela bir saniyede bir,
+        // yapılacak işlemleri ve ondan sonrada bu işlem bittikten sonra da ne olacağını belirten obje oluşturuyor.
+        //myTimer, countdowntimer'in özelliklerini kullanan bir nesnedir.
+       var startTime = eetStartTime?.text.toString()
+        object: CountDownTimer((startTime.toLong()+1)*1000,1000){
+            override fun onFinish() {
+                tvDisplaySeconds.text="DONE!"
+            }
+
+            override fun onTick(millisUntilFinished: Long) {
+                tvDisplaySeconds.text="${millisUntilFinished/1000}"
+            }
+
+        }.start()
     }
 }
